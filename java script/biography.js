9205c014c78191cda3b29c3b045d771899a166b1
be7shishirp <be7shishirp@gmail.com>
2025-10-04 08:25:11 +0545

    Create biography.js

---
+++ java script/biography.js
@@ -0,0 +1,3 @@
+// biography.js - small placeholder for astrology summary
+const astEl = document.getElementById('astrology-summary');
+astEl.innerHTML = `<p><em>Astrology note:</em> Sun/Moon/Rising details require birth time + place. Provided DOB: 2061-07-07 BS (2004-10-23 AD). For live rasifal, integrate a Nepali astrology API or paste daily text here.</p>`;
